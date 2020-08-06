a87630ebe1391a0b0bf427bbfefcf1d4dc6b369f
Harfeur <maxou3153@gmail.com>
2020-08-06 21:33:50 +0200

    update savedParticipations

--- modules/leetchi.js
+++ modules/leetchi.js
@@ -41,6 +41,8 @@ async function scrap() {
                 i++;
             }
 
+            savedParticipations = participations;
+
             if (newParticipations.length != 0)
                 obj.emit('newParticipations', newParticipations);
         }
