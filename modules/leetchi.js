d4b540f05248718f2a1dc2afaa70dece2b4b7bd6
Harfeur <maxou3153@gmail.com>
2020-08-18 16:17:17 +0200

    Mise à jour leetchi pour sauvegarder lastID + err

--- modules/leetchi.js
+++ modules/leetchi.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const EventEmitter = require('events');
+const { Client } = require('pg');
 
 const leetchID = "rE4Qjva5";
 const url = "https://www.leetchi.com/fr/Fundraising/Participations";
@@ -8,9 +9,31 @@ var obj = new EventEmitter.EventEmitter();
 
 var savedParticipations;
 
+const clientpg = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+});
+
+clientpg.connect();
+
 module.exports = obj;
 
 async function scrap() {
+    var stop = false;
+
+    var lastID = 0;
+    await clientpg.query(`SELECT value FROM vars WHERE name='leetchi';`)
+    .then(res => {lastID = parseInt(res.rows[0].value);})
+    .catch(err => {
+        console.log("Erreur Postgres SELECT");
+        console.error(err);
+        stop = true;
+    });
+
+    if (stop) return;
+
     const options = {
         method: "GET",
         headers: {
@@ -19,35 +42,51 @@ async function scrap() {
         },
         data: {
             hashId: leetchID,
-            lastId: 'undefined'
+            lastId: undefined
         },
         url
     }
 
-    const response = await axios(options);
+    var response;
+
+    await axios(options)
+    .then(res => {response = res})
+    .catch(err => {
+        console.log("ERREUR LEETCHI");
+        console.error(err);
+        stop = true;
+    });
+
+    if (stop) return;
+
     const participations = response.data.data;
-    if (participations != undefined) {
-        if (savedParticipations == undefined) {
-            savedParticipations = participations
-        } else {
-            let newParticipations = [];
-            let lastID = savedParticipations.data[0].id;
-            let fin = false;
-            let i = 0;
-            while (i < participations.data.length && !fin) {
-                if (participations.data[i].id != lastID)
-                    newParticipations.push(participations.data[i]);
-                else
-                    fin = true;
-                i++;
-            }
 
-            savedParticipations = participations;
-
-            if (newParticipations.length != 0)
-                obj.emit('newParticipations', newParticipations);
-        }
+    let fin = false;
+    let i = 0;
+    let newParticipations = [];
+    while (i < participations.data.length && !fin) {
+        if (participations.data[i].id > lastID)
+            newParticipations.push(participations.data[i]);
+        else
+            fin = true;
+        i++;
     }
+
+    
+
+    if (newParticipations.length != 0) {
+        console.log(newParticipations);
+        await clientpg.query(`UPDATE vars SET value = '${newParticipations[0].id}' WHERE name = 'leetchi';`)
+        .catch(err => {
+            console.log("Erreur Postgres UPDATE");
+            console.error(err);
+            stop = true;
+        });
+        if (stop) return;
+        console.log(`Envoi de ${newParticipations.length} nouveau(x) don(s)`);
+        obj.emit('newParticipations', newParticipations);
+    }  
+    
 
     var random = Math.floor(Math.random() * 3000);
     setTimeout(scrap, 5000 + random);
